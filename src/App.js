7c24bb3a0183c93b6c10684dba367d18323ecd83
Gab-byte4 <uliseschapov97@gmail.com>
2021-09-28 14:56:25 -0300

    finished main files

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Home from "./components/Home";
 import Header from "./components/Header";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Detail from "./components/Detail";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
-
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/detail">
             <Detail />
           </Route>
